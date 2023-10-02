908de0297e336ed2d29caa21345b47a3b4f73808
clbokea <clbo@kea.dk>
2023-10-02 14:17:41 +0200

    init

---
+++ src/main/java/ita3/jparelations/JpaRelationsApplication.java
@@ -0,0 +1,13 @@
+package ita3.jparelations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRelationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaRelationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ita3/jparelations/JpaRelationsApplicationTests.java
@@ -0,0 +1,13 @@
+package ita3.jparelations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRelationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
